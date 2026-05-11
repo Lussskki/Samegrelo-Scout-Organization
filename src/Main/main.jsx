0d65a0f6b0980dd61c91519e90bfa067f24dd860
Luka <lussskki@gmail.com>
2026-05-11 11:23:27 +0400

    fix, bug: Vite case sensitivity

--- src/Main/main.jsx
+++ src/Main/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App/App'
-import { Admin } from './Admin/Admin'
+
+import App from '../App/App'
+import { Admin } from '../Admin/Admin'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
